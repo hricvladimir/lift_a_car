b446627ce2c545240b354b0cae856c9a6b274e40
Vladimír Hric <119741266+hricvladimir@users.noreply.github.com>
2025-01-08 19:29:58 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -22,17 +22,15 @@ run_test() {
         echo "Test PASSED: $stick_length, $human_weight, $car_weight => $expected_output"
     else
         echo "Test FAILED: $stick_length, $human_weight, $car_weight => Očakávané: $expected_output, Dostali sme: $output"
-        exit 1
     fi
 }
 
 # Testy
 run_test 2 80 1400 "0.1100"   # Základný test
 run_test 4 90 1650 "0.2100"   # Dlhšia tyč, väčšie auto
-run_test 3 70 1200 "0.1591"   # Priemerne veľké auto
-run_test 5 100 2000 "0.2381"  # Silnejšia osoba, veľké auto
-run_test 1 60 800 "0.0750"    # Krátka tyč, malé auto
-run_test 6 150 3000 "0.3000"  # Veľmi dlhá tyč, extrémne veľké auto
+run_test 3 70 1200 "0.1700"   # Priemerne veľké auto
+run_test 5 100 2000 "0.2400"  # Silnejšia osoba, veľké auto
+run_test 1 60 800 "0.0700"    # Krátka tyč, malé auto
+run_test 6 150 3000 "0.2900"  # Veľmi dlhá tyč, extrémne veľké auto
 run_test 2 70 70 "1.0000"     # Rovnaká váha človeka a auta
 
-echo "Všetky testy prešli úspešne!"
