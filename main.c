227190eac7825fb00785fc974fe3cfb6fb892b9a
Vladimír Hric <119741266+hricvladimir@users.noreply.github.com>
2025-01-08 18:57:56 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#import <stdio.h>
+
+float lift_a_car(const int stick_length, const int human_weight, const int car_weight) {
+  return 0.0
+}
+
+int main() {
+  printf("%.4f\n", lift_a_car(2, 80, 1400));
+}
+
